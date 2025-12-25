942f5ea8d2a35a3892d18e4bb47215e74290c208
putbalqistina <putbalqistina@gmail.com>
2025-12-25 16:02:23 +0800

    Add files via upload

---
+++ try.py
@@ -0,0 +1,30 @@
+# main menu
+
+print ('Welcome to PotionMaker!')
+print ('In this game, you will be experimenting with the ingredients and come up with a mysterious potion!')
+print ()
+print ('These are the steps to play this game:')
+print ('1. Enter your nickname to save your individual progress.')
+print ('2. Choose your ingredients from the ingredients list given.')
+print ('3. After creating your potion, you can name it and see its ingredients list, rarity, effect and points obtained.')
+print ('4. With the points obtained, you can upgrade to the next level where more ingredients will be unlocked.')
+print ('5. You can update potion details, delete failed or unwanted recipes and view all discovered potion.')
+print()
+print ('Happy playing!')
+
+# user input
+print ()
+username = input('Enter your nickname:')
+print (f'Hello {username}!')
+print ()
+
+
+
+
+ 
+# ingredients list
+print ('Choose your ingredients:')
+print ()
+print ('Main ingredient   |   2nd ingredient   |   3rd ingredient')
+print ('---------------------------------------------------------')
+
